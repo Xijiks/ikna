0501b50a6d9a647ca83fd33915cbb6a92391a58d
German Nester <german_nester@mail.ru>
2025-11-19 21:13:29 -0600

    Fix guid selection not being case sensitive in API

--- server/services/guidToId.mjs
+++ server/services/guidToId.mjs
@@ -2,7 +2,7 @@ import { pool } from "../app.mjs";
 
 export default async function guidToId(guid, table) {
   if (!guid || !table) return null;
-  const query = `SELECT id FROM ${table} WHERE guid = ?`;
+  const query = `SELECT id FROM ${table} WHERE BINARY guid = ?`;
   const [result] = await pool.query(query, [guid]);
   // Checking decks with the same ID
   if (result.length === 0) {
